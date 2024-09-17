c574400f27383a8ee719e02d2eb5c4d1fa74ba76
Rodrigo Del Angel Gerardo <20210658@uthh.edu.mx>
2024-09-17 17:58:06 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'movil_user_mygardenllc',
+  webDir: 'dist'
+};
+
+export default config;
